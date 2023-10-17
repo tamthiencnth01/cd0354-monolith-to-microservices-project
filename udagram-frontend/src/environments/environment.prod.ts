c6f624bab2a4fb2a7b637fa6eaee05ab26734e38
ThienNLNT <tamthiencnth01@gmail.com>
2023-10-17 22:24:07 +0700

    last commit

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a45193a7000d0436a822265719cb394b-160740439.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
